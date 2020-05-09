8ee013a6711ba451153d8b3fb39a5440582cb9b1
Rutuja Kawade <56619747+rutujak24@users.noreply.github.com>
2020-05-09 15:35:41 +0530

    Update designerDoorMat.py

--- designerDoorMat.py
+++ designerDoorMat.py
@@ -39,9 +39,6 @@ Sample Designs
 """
 
 
-N, M = map(int,raw_input().split()) # More than 6 lines of code will result in 0 score. Blank lines are not counted.
-for i in xrange(0,N/2): 
-    print ('.|.'*i).rjust((M-2)/2,'-')+'.|.'+('.|.'*i).ljust((M-2)/2,'-')
-print 'WELCOME'.center(M,'-')
-for i in reversed(xrange(0,N/2)): 
-    print ('.|.'*i).rjust((M-2)/2,'-')+'.|.'+('.|.'*i).ljust((M-2)/2,'-')
+n, m = map(int,input().split())
+pattern = [('.|.'*(2*i + 1)).center(m, '-') for i in range(n//2)]
+print('\n'.join(pattern + ['WELCOME'.center(m, '-')] + pattern[::-1]))
