8057722be4e47048755b0f417414c4394f6727db
Rutuja Kawade <56619747+rutujak24@users.noreply.github.com>
2020-05-10 11:19:00 +0530

    Create splitJoin.py

---
+++ splitJoin.py
@@ -0,0 +1,24 @@
+'''Task
+You are given a string. Split the string on a " " (space) delimiter and join using a - hyphen.
+
+Input Format
+The first line contains a string consisting of space separated words.
+
+Output Format
+Print the formatted string as explained above.
+
+Sample Input
+
+this is a string   
+
+Sample Output
+
+this-is-a-string
+'''
+
+def split_and_join(line):
+    # write your code here
+    
+    T = line.split(" ")
+    x= "-".join(T)
+    return x
